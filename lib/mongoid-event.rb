ea1d80ca70467dbb5c9d7d8d78ce91a93a3c01af
Akeem Adeniji <Adelanwa.A.Adeniji@aexp.com>
2012-09-17 16:30:33 -0400

    initial commit

---
+++ lib/mongoid-event.rb
@@ -0,0 +1,7 @@
+require "mongoid-event/version"
+
+module Mongoid
+  module Event
+    # Your code goes here...
+  end
+end
---
+++ lib/mongoid-event/version.rb
@@ -0,0 +1,5 @@
+module Mongoid
+  module Event
+    VERSION = "0.0.1"
+  end
+end
